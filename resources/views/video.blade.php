4db8101c7ad3bc5e558938e64e7edf50d08e546b
cqkd6381 <cqkd6381@163.com>
2017-08-14 17:39:14 +0800

    use arrayBuffer where ajax

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -25,9 +25,9 @@
 <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.js"></script>
 <script type="text/javascript">
 	var video0 = document.createElement("video");
-	setInterval(function(){
-    	console.log(video0.readyState);
-    },100);
+	// setInterval(function(){
+ //    	console.log(video0.readyState);
+ //    },100);
 	function upload(){
 		var img_url = "http://zh.laravel.dev/pic";
 		// var img = document.createElement("img");
@@ -58,43 +58,59 @@
 	    var video_xhr = new XMLHttpRequest();
 	    console.log(video_xhr)
 	    video_xhr.open('GET', video_url, true);
-	    video_xhr.responseType = "blob";
+	    video_xhr.responseType = "arraybuffer";
 	    video_xhr.onload = function() {
 	        if (this.status == 200) {
-	            var blob = this.response;
-	            console.log(blob)
-	            var video = document.createElement("video");
-	            video.setAttribute("width", "320");
-				video.setAttribute("controls", "controls");
-				video.setAttribute("class", "ff");
-	            video.onload = function(e) {
-	                window.URL.revokeObjectURL(video.src); 
-	            };
-	            video.src = window.URL.createObjectURL(blob);
+	        	var arrayBuffer = this.response;
+	        	if(arrayBuffer){
+	        		var byteArray = new Uint8Array(arrayBuffer);
+	        		console.log(byteArray);
+	        		// for(var i=0;i<byteArray.byteLength;i++){
+	        		// 	console.log(i)
+	        		// }
+	        		var video = document.createElement("video");
+	        		video.setAttribute("width", "320");
+	        		video.onload = function(e) {
+		                window.URL.revokeObjectURL(video.src); 
+		            };
+		            video.src = window.URL.createObjectURL(arrayBuffer);
 
-	            var video2 = document.createElement("video");
-	            video2.setAttribute("width", "320");
-				video2.setAttribute("controls", "controls");
-				video2.setAttribute("class", "ff");
-				video2.src = 'http://zh.laravel.dev/1.mp4';
-				video2.muted = true;
+	        	}
 
-	            var video3 = document.querySelector("#video");
-	            video3.setAttribute("width", "320");
-				video3.setAttribute("controls", "controls");
-				video3.setAttribute("class", "ff");
-				video3.onload = function(e) {
-	                window.URL.revokeObjectURL(video3.src); 
-	            };
-	            video3.src = window.URL.createObjectURL(blob);
-	            // video3.src = 'http://zh.laravel.dev/vvv';
-	            // video3.muted = false; //初始化音量
-	            // alert(video3.muted);
-	            // video3.controls = "controls";
-	            video3.muted = false;
+	   //          var blob = this.response;
+	   //          console.log(blob)
+	   //          var video = document.createElement("video");
+	   //          video.setAttribute("width", "320");
+				// video.setAttribute("controls", "controls");
+				// video.setAttribute("class", "ff");
+	   //          video.onload = function(e) {
+	   //              window.URL.revokeObjectURL(video.src); 
+	   //          };
+	   //          video.src = window.URL.createObjectURL(blob);
+
+	   //          var video2 = document.createElement("video");
+	   //          video2.setAttribute("width", "320");
+				// video2.setAttribute("controls", "controls");
+				// video2.setAttribute("class", "ff");
+				// video2.src = 'http://zh.laravel.dev/1.mp4';
+				// video2.muted = true;
+
+	   //          var video3 = document.querySelector("#video");
+	   //          video3.setAttribute("width", "320");
+				// video3.setAttribute("controls", "controls");
+				// video3.setAttribute("class", "ff");
+				// video3.onload = function(e) {
+	   //              window.URL.revokeObjectURL(video3.src); 
+	   //          };
+	   //          video3.src = window.URL.createObjectURL(blob);
+	   //          // video3.src = 'http://zh.laravel.dev/vvv';
+	   //          // video3.muted = false; //初始化音量
+	   //          // alert(video3.muted);
+	   //          // video3.controls = "controls";
+	   //          video3.muted = false;
 	            
 	            $("#videocontainer").html(video);  
-	            $("#videocontainer2").html(video2);  
+	   //          $("#videocontainer2").html(video2);  
 	        }
 	    }
 	    video_xhr.send();
